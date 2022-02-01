141ffcd727f3a36f68f21c76d1e6b8882adde535
Bezzzzz <berylsoli@hotmail.co.uk>
2022-02-01 20:37:19 +0000

    passing tests just needs styling

--- src/index.js
+++ src/index.js
@@ -13,26 +13,24 @@ const todosAbsoluteFilePath = __dirname + todoFilePath;
 //Read todos from todos.json into variable
 let todos = require(todosAbsoluteFilePath); //the array of objects
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
 app.use(bodyParser.json()); // allows to send metadata to the header
 
 
-// ******THIS WORKS *********
 app.use("/content", express.static(path.join(__dirname, "public"))); //allows for static content from the html to be seen on localhost:8080/content
 
 
-// *********THIS WORKS **********
 app.get("/", (_, res) => {
-  // res.header("Content-Type", "application/html");
+  // res.header("Content-Type", "application/html"); doesn't work when this is commented in
   res.status(200);
   res.sendFile("/public/index.html", { root: __dirname });
   
 });
 
 
-// ********THIS WORKS- SHOWS ARRAY**********
 app.get('/todos', (_, res) => {
   res.header("Content-Type", "application/json");
   res.status(200);
@@ -40,15 +38,15 @@ app.get('/todos', (_, res) => {
 });
 
 
-// ***********WORKS***************
 //Add GET request with path '/todos/overdue'
 app.get("/todos/overdue", (req, res) => {
-   res.header("Content-Type", "application/json"); //never have space between the / otherwise it won't work
-     const date = new Date('29 January 2022 18:09 UTC')
-    //  console.log(date.toISOString()) -works
+  res.header("Content-Type", "application/json");                                                       //never have space between the / otherwise it won't work
+   
+  const date = new Date('29 January 2022 18:09 UTC')
+                                                                                                             //  console.log(date.toISOString()) -works
 
-    const overDueTodo = todos.filter((todo) => date.toISOString() > todo.due && todo.completed === false); //if the current date is greater than the todo due date and not completed return this 
-    // console.log(overDueTodo) //- //works shows the array overdue
+  const overDueTodo = todos.filter((todo) => date.toISOString() > todo.due && todo.completed === false); //if the current date is greater than the todo due date and not completed return this 
+                                                                                                           // console.log(overDueTodo) //- //works shows the array overdue
              res.send(overDueTodo);
              res.status(200);
   
@@ -57,19 +55,20 @@ app.get("/todos/overdue", (req, res) => {
   
   
 
-// ******************WORKS*******************************
+
 //Add GET request with path '/todos/completed'
 app.get ("/todos/completed", (req, res) => {
-  res.header("Content-Type", "application/json");
-     
-  // let currentDate = new Date('29 January 2022 18:09 UTC')
-  const completedTodo = todos.filter((todo) => todo.completed === true);  //filter method will bring all the data that matches the condition however if you use the .find it only shows the first data that matches and not the rest.
 
-    //  console.log(completedTodo) 
+res.header("Content-Type", "application/json");
+     
+const completedTodo = todos.filter((todo) => todo.completed === true);              //filter method will bring all the data that matches the condition however if you use the .find it only shows the first data that matches and not the rest.
+
    res.send(completedTodo); 
    res.status(200);     
 
 });
+
+
 
 
 
@@ -78,39 +77,47 @@ app.get ("/todos/completed", (req, res) => {
 //Add a new todo to the todo list 
 
 app.post("/todos", (req, res) => {
-      // console.log("New add: ", req.body); - shows added array in the terminal 
-      const newTodo = req.body;
-      // console.log("old todos", todos); 
+  const newTodo = req.body;
 
- if(true){
-     const newTodosArray = [...todos, newTodo]; //instead of modifying the current this creates a new array
-  
-    fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(newTodosArray)) //json.stringify changes the context to a string that json can read
-
-    res.header("Content-Type", "application/json"); 
-    res.send("Created new Todo");
-    res.status(201).end();
-  } 
-  else{  
+  // in the request body it should specifiy the due and name because the code without if statements allows for all todos to be created regardless of if the attribute is there.
+  // name and due are keys of req.body (request body)
+  // if (["name", "due"] !== Object.keys(newTodo)) {
+  if (newTodo.name === undefined || newTodo.due === undefined) {
+    res.status(400);
     res.send("Incorrect data submitted");
-    res.status(400).end();
 
+  } else {
+    newTodo.completed = false
+    newTodo.created = new Date().toISOString()
+
+    const newTodosArray = [...todos, newTodo];
+    //instead of modifying the current this creates a new array
+
+    fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(newTodosArray))               //json.stringify changes the context to a string that json can read
+
+    res.status(201);
+    res.send("Created new Todo");  
   }   
 });
 
 
 
-// ************THIS WORKS****************
-app.get("/todos/:id", (req,res) => {                              //parameter always has a : in front of it then followed by the name
-    res.header("Content-Type", "application/json"); 
-      const id = req.params.id;                                 //  console.log("the first todo id: ", req.params.id); - checking to see if it returns
-      const foundTodo = todos.find((todo) => todo.id == id);   //.find()- it will return one object that matches the criteria i am passing through, the .find must have a condition to it otherwise nothing is returned. 
 
-        if(foundTodo){
+
+
+// Add GET req with '/todos/:id'
+app.get("/todos/:id", (req,res) => {                                     //parameter always has a : in front of it then followed by the name
+res.header("Content-Type", "application/json"); 
+      
+const id = req.params.id;                                                //  console.log("the first todo id: ", req.params.id); - checking to see if it returns
+const foundTodo = todos.find((todo) => todo.id == id);                   //.find()- it will return one object that matches the criteria i am passing through, the .find must have a condition to it otherwise nothing is returned. 
+
+    if(foundTodo){
             res.send(foundTodo);
             res.status(200)
          } 
-         else{
+
+    else{
            res.status(404);
            res.send("No todo found");
          }                                                  
@@ -119,33 +126,33 @@ app.get("/todos/:id", (req,res) => {                              //parameter al
 
 
 
-// ************************THIS WORKS********************************
-//Add PATCH request with path '/todos/:id
-//either edit the name, the due date OR edit the name AND the due date
 
+
+
+//Add PATCH request with path '/todos/:id
 app.patch("/todos/:id", (req, res) => {
   const id = req.params.id
   const attributes = req.body
 
-  const UpdatedTodo = todos.find((todo) => todo.id === id);           //id, attributes, todos.name
-  // console.log(UpdatedTodo.name)
-  // console.log(attributes.name) 
+  const UpdatedTodo = todos.find((todo) => todo.id === id);                 //id, attributes, todos.name
 
-  if (UpdatedTodo) {                                          //if updatedTodo is different from undefined (true) then proceed with the if statements otherwise show the else statement.
-      if (attributes.name !== undefined) {                      //if the name is not given in the patch request in postman then show undefined in the terminal. If it is given then update the name in postman  
-        UpdatedTodo.name = attributes.name                     // if attributes.name is equal then set it 
+
+  if (UpdatedTodo) {                                                       //if updatedTodo is different from undefined (true) then proceed with the if statements otherwise show the else statement.
+      if (attributes.name !== undefined) {                                 //if the name is not given in the patch request in postman then show undefined in the terminal. If it is given then update the name in postman  
+        UpdatedTodo.name = attributes.name                                 // if attributes.name is equal then set it 
       }
-      if (attributes.due !== undefined) {                      // due date is undefined because its not coming in the request body (attributes)
+      if (attributes.due !== undefined) {                                  // due date is undefined because its not coming in the request body (attributes)
         UpdatedTodo.due = attributes.due
       }
 
-    fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(todos)) //first argument is the path and the second is the content
+    fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(todos))          //first argument is the path and the second is the content
 
     res.status(200);
     res.send(UpdatedTodo);
 
-  } else {
-    // if UpdatedTodo is undefined, i.e., does not exit with that id
+  } 
+  else {
+                                                                            // if UpdatedTodo is undefined, i.e., does not exit with that id
     res.status(400);
     res.send("That todo does not exist");
   }
@@ -154,27 +161,28 @@ app.patch("/todos/:id", (req, res) => {
 
 
 
+
+
+
 //Add POST request with path '/todos/:id/complete
-// Update todo, set attribute complete to true
 //PUT and POST methoud allows for updating exsisting data 
 
 app.post("/todos/:id/complete", (req, res) => {
    const id = req.params.id
    const CompleteTodo = todos.find((todo) => todo.id ==id);
-  //  console.log(CompleteTodo)
-
-  if(CompleteTodo){
-      CompleteTodo.completed = true
   
 
-      fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(todos))
-      
-      res.status(200);
-      res.send(CompleteTodo)
+  if(CompleteTodo){
+        CompleteTodo.completed = true
+
+        fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(todos))
+        
+        res.status(200);
+        res.send(CompleteTodo)
     }
     
   else {
-    // if UpdatedTodo is undefined, i.e., does not exit with that id
+  
     res.status(400);
     res.send("Bad Request) if invalid id");
   }
@@ -182,12 +190,12 @@ app.post("/todos/:id/complete", (req, res) => {
 
 
 
-//Add POST request with path '/todos/:id/undo
 
+//Add POST request with path '/todos/:id/undo
 app.post("/todos/:id/undo", (req, res) => {
   const id = req.params.id
+  
   const UndoTodo = todos.find((todo) => todo.id ==id);
- //  console.log(CompleteTodo)
 
  if(UndoTodo){
      UndoTodo.completed = false
@@ -199,7 +207,7 @@ app.post("/todos/:id/undo", (req, res) => {
    }
    
  else {
-   // if UpdatedTodo is undefined, i.e., does not exit with that id
+                                                                           // if UpdatedTodo is undefined, i.e., does not exit with that id
    res.status(400);
    res.send("Bad Request) if invalid id");
  }
@@ -208,32 +216,25 @@ app.post("/todos/:id/undo", (req, res) => {
 
 
 
-//  ****PASSING TEST*****
 // Add DELETE request with path '/todos/:id
 app.delete("/todos/:id", (req, res) => {
-  // console.log("this is a request id ", req.params.id);
+                                                                                  // console.log("this is a request id ", req.params.id);
   const id = req.params.id
-  // console.log(id)
-  
-  const todoexsists= todos.some(todo => todo.id === id) //.some() passes back true or false
-
-     
-  if(todoexsists){ //show the deleted todo?
-  const RemovedTodoFromCliet = todos.filter((todo) => todo.id !== id); //Only return the id's from the json file that are not equal to the id being called by the client. 
-  
-  //  console.log("everything but learn todo", RemovedTodoFromCliet); 
-  
-   console.log(todosAbsoluteFilePath)
-   fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(RemovedTodoFromCliet), err => {
-    if (err) {
-      console.error(err)
-      return
-    }}); 
-    res.header("Content-Type", "application/json"); 
-    res.status(200).end();
-    
-  } 
-  else{  
+                                                                                  // console.log(id)
+  const todoexsists= todos.some(todo => todo.id === id)                          //.some() passes back true or false
+                         
+  if(todoexsists){ 
+    const RemovedTodoFromCliet = todos.filter((todo) => todo.id !== id);         //Only return the id's from the json file that are not equal to the id being called by the client. 
+    console.log(todosAbsoluteFilePath)
+    fs.writeFileSync(todosAbsoluteFilePath, JSON.stringify(RemovedTodoFromCliet), err => {
+      if (err) {
+        console.error(err)
+        return
+      }}); 
+      res.header("Content-Type", "application/json"); 
+      res.status(200).end();
+    } 
+  else {  
     res.status(400).end();
   }   
 });
